ad5b23244686c63f7f2d0364207f8946d9e49e03
Ayoub Boulila <ayoubboulila@gmail.com>
2016-09-14 10:53:58 +0100

    startup script

---
+++ startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#################################################
+# Author: Ayoub Boulila <ayoubboulila@gmail.com>
+# script for starting up snap tribe based on 
+# ENV variables
+# 
+# 
+################################################
+
+run(){
+bash /opt/snap/bin/snapd --tribe -t $TRUST --tribe-port $TRIBEPORT --api-port $APIPORT --tribe-node-name $HOST --tribe-seed $SEEDIP:SEEDPORT
+
+
+
+}
+
+
+
+case "$1" in
+  (run)
+    run
+    exit 0
+    ;;
+
+  (*)
+    echo -e "$G Usage: sudo bash $0 {run} $N"
+    exit 2
+    ;;
+esac
