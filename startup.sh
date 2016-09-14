7f19a2fd9b4b54a2632cdd3f665c102bc47b171a
Ayoub Boulila <ayoubboulila@gmail.com>
2016-09-14 20:33:18 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,13 +9,13 @@
 ################################################
 
 run(){
-local mode = $MODE
-if [ mode -eq "master" ] 
+local mode=$MODE
+if [ mode=="master" ] 
 then
 echo "AYB [debug]: starting tribe master node"
 snapd --tribe -t $TRUST --api-port $APIPORT --tribe-node-name $HOST
 
-elif [ mode -eq "member" ] 
+elif [ mode=="member" ] 
 then
 echo "AYB [debug]: starting member node"
 snapd --tribe -t $TRUST --tribe-port $TRIBEPORT --tribe-addr $TRIBEADDR --api-port $APIPORT --tribe-node-name $HOST --tribe-seed $SEEDIP:$SEEDPORT
