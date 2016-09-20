093295c9d484a36d7fbe5e2f8890a46da35fb743
Ayoub Boulila <ayoubboulila@gmail.com>
2016-09-20 14:59:53 +0100

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#################################################
+# Author: Ayoub Boulila <ayoubboulila@gmail.com>
+# script for starting up snap tribe based on 
+# ENV variables
+# 
+# 
+################################################
+
+run(){
+
+if [ "${MODE}" == "master" ] 
+then
+echo "AYB [debug]: starting tribe master node"
+snapd --tribe -t $TRUST --api-port $APIPORT --tribe-node-name $HOST
+
+elif [ "${MODE}" == "member" ] 
+then
+echo "AYB [debug]: starting member node"
+snapd --tribe -t $TRUST --tribe-port $TRIBEPORT --tribe-addr $TRIBEADDR --api-port $APIPORT --tribe-node-name $HOST --tribe-seed $SEEDIP:$SEEDPORT
+
+fi
+}
+
+
+
+case "$1" in
+  (run)
+    run
+    exit 0
+    ;;
+
+  (*)
+    echo -e "$G Usage: sudo bash $0 {run} $N"
+    exit 2
+    ;;
+esac
